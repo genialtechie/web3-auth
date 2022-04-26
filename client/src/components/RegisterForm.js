a66b95605cb33eaca842c448a76cf37046de57d6
Haleem Bello <thehaleembello@gmail.com>
2022-04-26 00:15:02 +0000

    created register form component

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,1 +1,11 @@
+import React, {useRef} from 'react'
 import { Form, Button } from 'react-bootstrap';
+
+
+const RegisterForm = () => {
+  return (
+    <div>RegisterForm</div>
+  )
+}
+
+export default RegisterForm
