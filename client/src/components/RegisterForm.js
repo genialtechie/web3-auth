829d7882a98f2f6e56453f96fca8ef5abe983928
Haleem Bello <thehaleembello@gmail.com>
2022-04-26 21:19:57 +0000

    setting up database

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -3,18 +3,45 @@ import { Form, Button } from 'react-bootstrap';
 
 
 const RegisterForm = () => {
+  const nameRef = useRef();
+  const addressRef = useRef();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const data = new FormData();
+
+    const name = nameRef.current.value;
+    const  address = addressRef.current.value;
+    data.append('name', name)
+    data.append('address', address)
+
+    const response = await fetch('http://localhost:5000/api/users/register', {
+      method: 'POST',
+      body: data
+    })
+
+    console.log(response)
+  }
   return (
     <div className='w-70 p-6 flex justify-center vh-100 items-center'>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formName">
           <Form.Label>Alias</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name or alias" />
+          <Form.Control 
+          required 
+          type="text" 
+          placeholder="Enter your name or alias" 
+          ref={nameRef}/>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="formAddress">
           <Form.Label>Wallet Address</Form.Label>
-          <Form.Control type="text" placeholder="Enter your wallet id" />
+          <Form.Control 
+          required 
+          type="text" 
+          placeholder="Enter your wallet id" 
+          ref={addressRef}/>
         </Form.Group>
-        <Button>Register</Button>
+        <Button variant='outline-dark' className='btn-dark' type='submit'>Register</Button>
       </Form>
     </div>
   )
