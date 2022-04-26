e8d55840ec8509594c5919deb79925f83dcf889e
genialtechie <thehaleembello@gmail.com>
2022-04-25 22:27:33 -0400

    styled register form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -4,7 +4,19 @@ import { Form, Button } from 'react-bootstrap';
 
 const RegisterForm = () => {
   return (
-    <div>RegisterForm</div>
+    <div className='w-70 p-6 flex justify-center vh-100 items-center'>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Alias</Form.Label>
+          <Form.Control type="text" placeholder="Enter your name or alias" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Wallet Address</Form.Label>
+          <Form.Control type="text" placeholder="Enter your wallet id" />
+        </Form.Group>
+        <Button>Register</Button>
+      </Form>
+    </div>
   )
 }
 
