fd717072a23851e6868e9f596bcb2f7b12301f03
Haleem Bello <thehaleembello@gmail.com>
2022-04-26 21:18:49 +0000

    setting up database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const app = express()
+
+app.use(cors());
+
+const url = process.env.MONGO_ATLAS
+console.log(url);
+//mongoose.connect(url);
 
 app.get('/', function (req, res) {
   res.send('Hello World')
