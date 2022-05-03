e950c92b3828853d98c57b3c9275c956543d09a8
genialtechie <thehaleembello@gmail.com>
2022-05-03 06:49:05 -0400

    register route

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/register' element={<Register/>} ></Route>
+        <Route path='/login' element={<Register/>} ></Route>
       </Routes>
       
     </BrowserRouter>
